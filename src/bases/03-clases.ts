2e77b1b83e0586efbed07e623ac8b61739292865
Arcos2819 <arcosmunozemiliano719@gmail.com>
2026-02-25 16:38:39 -0500

    Se agrego una nueva caracteristica al objeto

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -1,17 +1,17 @@
 // export class Usuario {
-//     //PROPIEDADES
-//     nombre: string;
-//     edad: number;
+//   //   //PROPIEDADES
+//   nombre: string;
+//   edad: number;
 
-//     //METODOS
-//     constructor(nombre: string, edad: number) {
-//         this.nombre = nombre;
-//         this.edad = edad;
-//     }
+//   //METODOS
+//   constructor(nombre: string, edad: number) {
+//     this.nombre = nombre;
+//     this.edad = edad;
+//   }
 
-//     saludar(): void{
-//         console.log(`Hola, soy ${this.nombre} y tengo ${this.edad} años.`);
-//     }
+//   saludar(): void {
+//     console.log(`Hola, soy ${this.nombre} y tengo ${this.edad} años.`);
+//   }
 // }
 
 // //Crearun objeto tipo Usuario
@@ -19,12 +19,16 @@
 // userClass.saludar();
 
 export class Usuarios {
-    //METODOS
-    constructor(public readonly nombre: string, public readonly edad: number) {}
-    saludar(): string{
-        return (`Hola, soy ${this.nombre} y tengo ${this.edad} años.`);
-    }   
+  //METODOS
+  constructor(
+    public readonly nombre: string,
+    public readonly edad: number,
+    public readonly telefono?: string
+  ) {}
+  saludar(): string {
+    return `Hola, soy ${this.nombre} y tengo ${this.edad} años. y mi numero de telefono es ${this.telefono}`;
+  }
 }
 
 //Crear un objeto tipo Usuario
-export const userClass = new Usuarios("Diego", 30);
+export const userClass = new Usuarios("Diego", 30, 314 6798463);
